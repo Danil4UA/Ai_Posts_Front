15800878181293e3279db346bd6b04493dde1e91
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2024-11-09 14:46:13 +0200

    updated editor

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL_BACK = "http://localhost:5001"
-// export const API_BASE_URL_BACK = "https://ai-posts-back.onrender.com"
+// export const API_BASE_URL_BACK = "http://localhost:5001"
+export const API_BASE_URL_BACK = "https://ai-posts-back.onrender.com"
